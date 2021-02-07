da31be12b59720eab68f779ac4c170df11d4140c
Diyor Mukhamedkhanov <d.mukhamedkhanov98@gmail.com>
2021-02-07 22:26:39 +0500

    MongoDB connection added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
-const app = express()
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
+
+const app = express()
+
 
 app.get('/', (req, res) => {
     res.send('API is running...')
